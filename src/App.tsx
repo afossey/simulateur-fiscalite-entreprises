e831e2b3ffb292540b70a8c4736ef083eda7984d
Antoine Fossey <fossey.antoine@gmail.com>
2019-04-06 08:29:58 +0200

    Add react router, material ui, mobx-state-tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "@material-ui/core";
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Button variant={"raised"}>Test button</Button>
         </header>
       </div>
     );
