cc277bd7cd85815cbde08299e46faefb056c0693
Antoine Fossey <fossey.antoine@gmail.com>
2019-04-06 19:37:09 +0200

    Add model draft for AE, add moment

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,42 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Button} from "@material-ui/core";
+import {AEBusiness, AEConfig, AEStore, BusinessType} from "./model";
 
 class App extends Component {
+  constructor(props: any) {
+    super(props);
+
+    const defaultBusiness = AEBusiness.create({
+      type: BusinessType.SERVICES_LIBERAL,
+      nature: 'BNC'});
+
+    const AE2019Parameters = {
+      effectiveYear: '2019',
+      businesses: [
+        AEBusiness.create({
+          type: BusinessType.BUY_SELL,
+          nature: 'BIC'}),
+        AEBusiness.create({
+          type: BusinessType.FURNISHED_RENTAL_CLASSED_FOR_TOURISM,
+          nature: 'BIC'}),
+        AEBusiness.create({
+          type: BusinessType.SERVICES_LIBERAL,
+          nature: 'BNC'}),
+        AEBusiness.create({
+          type: BusinessType.RENTAL,
+          nature: 'BIC'})
+      ]
+    };
+    const aeStore = AEStore.create({
+      businessData: defaultBusiness,
+      config: AEConfig.create(AE2019Parameters)
+    });
+
+    aeStore.financialData.setAnnualRevenueWithoutTaxes(70000);
+    console.log(aeStore.incomeTaxForYear());
+  }
+
   render() {
     return (
       <div className="App">
---
+++ src/model.tsx
@@ -0,0 +1,196 @@
+import {types} from "mobx-state-tree";
+import moment from "moment";
+
+export enum BusinessType {
+  FURNISHED_RENTAL_CLASSED_FOR_TOURISM = 'FURNISHED_RENTAL_CLASSED_FOR_TOURISM',
+  RENTAL = 'RENTAL',
+  BUY_SELL = 'BUY_SELL',
+  SERVICES_LIBERAL = 'SERVICES_LIBERAL'
+}
+
+export const IncomeTaxScale = types.model({
+      firstScale: 9964,
+      firstScaleRate: 0,
+      secondScale: 27519,
+      secondScaleRate: 0.14,
+      thirdScale: 73779,
+      thirdScaleRate: 0.3,
+      fourthScale: 156244,
+      fourthScaleRate: 0.41,
+      beyondRate: 0.45
+    }
+).views( self => ({
+    computeIncomeTax(taxeableIncome: number): number {
+      let incomeTax = 0;
+
+      if(taxeableIncome < self.firstScale) {
+        incomeTax += taxeableIncome * self.firstScaleRate;
+        return incomeTax;
+      } else {
+        incomeTax += self.firstScale * self.firstScaleRate;
+      }
+
+      if(taxeableIncome < self.secondScale) {
+        incomeTax += (taxeableIncome - self.firstScale) * self.secondScaleRate;
+        return incomeTax;
+      } else {
+        incomeTax += (self.secondScale - self.firstScale) * self.secondScaleRate;
+      }
+
+      if(taxeableIncome < self.thirdScale) {
+        incomeTax += (taxeableIncome - self.secondScale) * self.thirdScaleRate;
+        return incomeTax;
+      } else {
+        incomeTax += (self.thirdScale - self.secondScale) * self.thirdScaleRate;
+      }
+
+      if(taxeableIncome < self.fourthScale) {
+        incomeTax += (taxeableIncome - self.thirdScale) * self.fourthScaleRate;
+        return incomeTax;
+      } else {
+        incomeTax = (self.fourthScale - self.thirdScale) * self.fourthScaleRate;
+      }
+      incomeTax += (taxeableIncome - self.fourthScale) * self.beyondRate;
+
+      return incomeTax;
+    }
+}));
+
+export const AEBusiness = types.model({
+      type: types.enumeration(Object.values(BusinessType)),
+      nature: types.union(types.literal('BIC'), types.literal('BNC'))
+    }
+).views(self => ({
+      get taxeableIncomePercent(): number {
+        if(self.type === (BusinessType.BUY_SELL || BusinessType.FURNISHED_RENTAL_CLASSED_FOR_TOURISM)) return (1 - 0.71);
+        if(self.nature === 'BIC') return 0.5;
+        else return (1- 0.34);
+      },
+      get socialCharges(): number {
+        if(self.type === BusinessType.BUY_SELL) return 0.128;
+        if(self.type === BusinessType.FURNISHED_RENTAL_CLASSED_FOR_TOURISM) return 0.06;
+        else return 0.22;
+      },
+      get vflRate(): number {
+        if(self.type.BUY_SELL) return 0.01;
+        if(self.nature === 'BIC') return 0.017;
+        else return 0.022;
+      }
+    })
+);
+
+export const Accre = types.model({
+  firstYearMultiplier: 0.75,
+  secondYearMultiplier: 0.5,
+  thirdYearMultiplier: 0.25
+});
+
+export const AEConfig = types.model({
+  // Legal year of the config
+  effectiveYear: types.string,
+
+  // ACCRE reduction rates
+  accre: types.optional(Accre, {}),
+
+  incomeTaxScale: types.optional(IncomeTaxScale, {}),
+
+  // Possible businesses for AE
+  businesses: types.array(AEBusiness),
+});
+
+export const FinancialData = types
+  .model({
+    annualRevenueWithoutTaxes: 0,
+    charges: 0
+  })
+  .actions(self => ({
+      setAnnualRevenueWithoutTaxes(newAnnualRevenueWithoutTaxes: number) {
+        self.annualRevenueWithoutTaxes = newAnnualRevenueWithoutTaxes;
+      },
+      setCharges(newCharges: number) {
+        self.charges = newCharges;
+      }
+    })
+  );
+
+export const CompanyData = types
+  .model( {
+    creationDate: new Date()
+  })
+  .actions( self => ({
+      setCreationDate(newCreationDate: Date) {
+        self.creationDate = newCreationDate;
+      }
+    })
+  )
+  .views( self => ({
+    get isFirstYear(): boolean {
+      return moment(self.creationDate).year() === moment().year();
+    },
+    get isSecondYear(): boolean {
+      return moment(self.creationDate).add(1, 'years').year() === moment().year();
+    },
+    get isThirdYear(): boolean {
+      return moment(self.creationDate).add(2, 'years').year() === moment().year();
+    },
+    get creationDayOfTheYear(): number {
+      return moment(self.creationDate).dayOfYear();
+    }
+  }));
+
+export const AEStore = types
+  .model( {
+    financialData: types.optional(FinancialData, {}),
+    companyData: types.optional(CompanyData, {}),
+    businessData: AEBusiness,
+    hasVFL: false,
+    hasACCRE: false,
+    config: AEConfig
+  })
+  .actions( self => ({
+        setOptionVFL(newHasVFL: boolean) {
+          self.hasVFL = newHasVFL;
+        },
+        setOptionACCRE(newHasACCRE: boolean) {
+          self.hasACCRE = newHasACCRE;
+        },
+        selectBusiness(businessType: BusinessType) {
+          const business = self.config.businesses.find(business => business.type === businessType);
+          if(business) self.businessData = business;
+          else console.error('Unsupported Business Type');
+        }
+      })
+  )
+  .views(self => {
+      const socialChargesForYear = function(): number {
+        if(self.hasACCRE) {
+          return self.financialData.annualRevenueWithoutTaxes * accreSocialCharges();
+        } else {
+          return self.financialData.annualRevenueWithoutTaxes * self.businessData.socialCharges;
+        }
+      };
+      const accreSocialCharges = function(): number {
+          if(self.companyData.isFirstYear) {
+            return self.config.accre.firstYearMultiplier * self.businessData.socialCharges;
+          }
+          if(self.companyData.isSecondYear) {
+            return self.config.accre.secondYearMultiplier * self.businessData.socialCharges;
+          }
+          if(self.companyData.isThirdYear) {
+            return self.config.accre.thirdYearMultiplier * self.businessData.socialCharges;
+          }
+          return self.businessData.socialCharges;
+      };
+      const taxeableIncome = function(): number {
+        return self.financialData.annualRevenueWithoutTaxes * self.businessData.taxeableIncomePercent;
+      };
+      const incomeTaxForYear = function(): number {
+         if(self.hasVFL) {
+           return self.businessData.vflRate * self.financialData.annualRevenueWithoutTaxes;
+         } else {
+           return self.config.incomeTaxScale.computeIncomeTax(taxeableIncome());
+         }
+      };
+      return { socialChargesForYear, accreSocialCharges, incomeTaxForYear, taxeableIncome};
+    }
+  );
